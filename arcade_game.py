365ae732fd057f6d886833c179d2bf65ef7f631e
262267 <81594625+262267@users.noreply.github.com>
2021-06-04 00:56:09 +0200

    Update dźwiękowy
    - dodanie funkcji włączania/ wyłączania muzyki i efektów muzycznych
    - poprawki stylistyczne kodu 
    - poprawne działanie zmiany poziomów trudności ( nie pamiętam, czy ostatnio to pisałem )

--- arcade_game.py
+++ arcade_game.py
@@ -2,11 +2,13 @@ import pygame
 from pygame.locals import *
 import pygame_menu
 import random
+from pygame_menu import sound
 
 
 
 def arcade_ball_movement():
     global arcade_ball_speed_x, arcade_ball_speed_y, arcade_paddle_speed_x, score_value, lifes_value, best_value
+
     arcade_ball.y -= arcade_ball_speed_y
     arcade_ball.x -= arcade_ball_speed_x
 
@@ -35,12 +37,14 @@ def arcade_ball_movement():
         arcade_ball_speed_y = 0
         arcade_ball.x = arcade_paddle.x + 25
         arcade_ball.y = arcade_paddle.y - 30
-        pygame.mixer.music.load("roblox.mp3")
+        pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                "graphics_and_sounds\\roblox.mp3")
         pygame.mixer.music.play()
         pygame.mixer.music.rewind()
         lifes_value -= 1
         if lifes_value == 0:
-            pygame.mixer.music.load("end_game.wav")
+            pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                    "graphics_and_sounds\\end_game.wav")
             pygame.mixer.music.play()
             pygame.mixer.music.rewind()
             if score_value > best_value:
@@ -67,25 +71,29 @@ def draw_rectangulars():
             # ball collision with top rectangular size
             if abs(r4.bottom - arcade_ball.top) < collision_tolerance:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
                 score_value += 1
             if abs(r4.left - arcade_ball.right) < collision_tolerance:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
                 score_value += 1
             if abs(r4.right - arcade_ball.left) < collision_tolerance:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
                 score_value += 1
             if abs(r4.top - arcade_ball.bottom) < collision_tolerance:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
                 score_value += 1
@@ -94,19 +102,22 @@ def draw_rectangulars():
         if arcade_ball.colliderect(r3):
             if arcade_ball.top >= r3.top:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
         if arcade_ball.colliderect(r2):
             if arcade_ball.top >= r2.top:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
         if arcade_ball.colliderect(r1):
             if arcade_ball.top >= r1.top:
                 arcade_ball_speed_y *= -1
-                pygame.mixer.music.load("click.wav")
+                pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                                        "graphics_and_sounds\\click.wav")
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
 
@@ -120,7 +131,8 @@ scoreX = 640
 scoreY = 400
 
 def show_score():
-    user_score_view = fontObj.render("Score: " + str(score_value), True, (255, 255, 255))
+    user_score_view = fontObj.render("Score: " + str(score_value),
+                                     True, (255, 255, 255))
     screen.blit(user_score_view, (scoreX, scoreY))
 
 # life
@@ -130,7 +142,8 @@ lifeX = 640
 lifeY = 430
 
 def show_lifes():
-    user_lifes_view = fontObj1.render("Lifes: " + str(lifes_value), True, (255,255,255))
+    user_lifes_view = fontObj1.render("Lifes: " + str(lifes_value),
+                                      True, (255,255,255))
     screen.blit(user_lifes_view, (lifeX, lifeY))
 
 # best score
@@ -153,7 +166,8 @@ def game_over():
     if lifes_value == 0:
         end_view = fontObj2.render("Game Over! Your total score: " + str(score_value),
                                    True, (255, 255, 255))
-        end_view1 = fontObj2.render("Click SPACE and try again!", True, (255, 255, 255))
+        end_view1 = fontObj2.render("Click SPACE and try again!",
+                                   True, (255, 255, 255))
         screen.blit(end_view, (endX, endY))
         screen.blit(end_view1, (endX1, endY1))
         score_value = 0
@@ -173,9 +187,11 @@ color2 = (255,255,255)
 color1 = (255,0,0)
 
 # Title and Icon
-my_image = pygame.image.load('tło.png')
+my_image = pygame.image.load('C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\'
+                             'graphics_and_sounds\\tło.png')
 pygame.display.set_caption("arcade game created by Jakub Muzyka")
-icon = pygame.image.load('arcanoid.png')
+icon = pygame.image.load('C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\'
+                         'graphics_and_sounds\\arcanoid.png')
 pygame.display.set_icon(icon)
 
 #arcanoid
@@ -188,11 +204,14 @@ arcade_ball_speed_x = 0
 arcade_ball_speed_y = 0
 
 # adding music to menu
-begin = pygame_menu.sound.Sound()
-# begin.set_sound(pygame_menu.sound.SOUND_TYPE_EVENT, "menu_music.wav")
-pygame.mixer.music.load("menu_music.wav")
+begin = sound.Sound()
+pygame.mixer.music.load("C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\"
+                         "graphics_and_sounds\\menu_music.wav")
 pygame.mixer.music.play(-1)
-begin.set_sound(pygame_menu.sound.SOUND_TYPE_WIDGET_SELECTION, "click.wav")
+begin.set_sound(pygame_menu.sound.SOUND_TYPE_WIDGET_SELECTION, "C:\\Users\\kubam\\OneDrive\\Desktop\\game\\"
+                                                               "my_python_game\\graphics_and_sounds\\click.wav")
+# Disable a sound
+begin.set_sound(pygame_menu.sound.SOUND_TYPE_ERROR, None)
 
 # create a manu for our game
 font = pygame_menu.font.FONT_8BIT
@@ -216,47 +235,41 @@ menu = pygame_menu.Menu(title='welcome to the arcanoid game!',
 
 menu.set_sound(begin, recursive=True)
 myimage = pygame_menu.baseimage.BaseImage(
-    image_path= "C:\\Users\\kubam\\OneDrive\\Desktop\\python_game\\tło.png"
+    image_path= "C:\\Users\\kubam\\OneDrive\\Desktop\\game\\my_python_game\\graphics_and_sounds\\tło.png"
 )
 
 def main_background():
     myimage.draw(screen)
 
 # create a settings
-# def sound_settings(value, enabled):
-#     if enabled:
-#         menu.set_sound(begin, recursive=True)
-#         print("Menu sounds were enabled")
-#     else:
-#         menu.set_sound(None, recursive=True)
-#         print("Menu sounds were disabled")
-#
-# def sound_effects_settings():
-#     pass
-settings_menu = pygame_menu.themes.Theme(
-                            widget_font=font1,
-                            background_color=(255, 255, 255, 0),
-                            widget_padding=25,
-                            readonly_color=(255,255,255),
-                            widget_font_color = (255,255,255),
-                            widget_font_size=24,
-                            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
-                            title_background_color=(0,0,0)
-                            )
+def sound_settings(value, sound_set):
+    if sound_set:
+        pygame.mixer.music.unpause()
+    else:
+        pygame.mixer.music.pause()
+
+def sound_effects_settings(value1, sound_effect_set):
+    if sound_effect_set:
+        menu.set_sound(begin, recursive=True)
+    else:
+        menu.set_sound(None, recursive=True)
+
+settings_menu = pygame_menu.themes.Theme(widget_font=font1,
+                                         background_color=(255, 255, 255, 0),
+                                         widget_padding=25,
+                                         readonly_color=(255,255,255),
+                                         widget_font_color = (255,255,255),
+                                         widget_font_size=24,
+                                         title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
+                                         title_background_color=(0,0,0)
+                                         )
 
 settings = pygame_menu.Menu(height=600,
                             width=800,
                             theme=settings_menu,
                             title='Settings'
                             )
-settings.add.toggle_switch(title='Sound:',
-                           default=True,
-                           # onchange=sound_settings
-                           )
-settings.add.toggle_switch('Sound effects:',
-                           True,
-                           # onchange=sound_effects_settings
-                           )
+
 def add_settings():
     settings.add.vertical_margin(50)
     settings.add.button(
@@ -266,16 +279,15 @@ def add_settings():
     )
 
 # create a Author button
-author_button = pygame_menu.themes.Theme(
-                            widget_font=font1,
-                            background_color=(255, 255, 255, 0),
-                            widget_padding=25,
-                            readonly_color=(255,255,255),
-                            widget_font_color = (255,255,255,0),
-                            widget_font_size=34,
-                            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
-                            title_background_color=(0,0,0)
-                            )
+author_button = pygame_menu.themes.Theme(widget_font=font1,
+                                         background_color=(255, 255, 255, 0),
+                                         widget_padding=25,
+                                         readonly_color=(255,255,255),
+                                         widget_font_color = (255,255,255,0),
+                                         widget_font_size=34,
+                                         title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
+                                         title_background_color=(0,0,0)
+                                         )
 
 author = pygame_menu.Menu(height=600,
                           width=800,
@@ -284,7 +296,7 @@ author = pygame_menu.Menu(height=600,
                           )
 
 author.add.vertical_margin(1)
-author.add.label('Introduction:',
+author.add.label(title='Introduction:',
                  align=pygame_menu.locals.ALIGN_LEFT,
                  font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
                  font_size=44
@@ -297,7 +309,7 @@ info = ('Witam wszystkich czytających tę wiadomość! Znajdujemy się w grze, 
         '"Instruction", gdzie dokładnie omówię zasady. Mam nadzieje, że spędzicie miło czas,'
         ' grając w moją grę, pozdrawiam! :)')
 
-author_info = author.add.label(info,
+author_info = author.add.label(title=info,
                                max_char=50,
                                align=pygame_menu.locals.ALIGN_CENTER,
                                margin=(29, 1),
@@ -319,29 +331,28 @@ for line in author_info:
     line.set_max_width(400)
 
 # create instruction button
-instruction_button = pygame_menu.themes.Theme(
-                            widget_font=font1,
-                            background_color=(0, 0, 0, 0),
-                            widget_padding=25,
-                            readonly_color=(255,255,255),
-                            widget_font_color = (255,255,255),
-                            widget_font_size=24,
-                            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
-                            title_background_color=(0,0,0)
-                            )
+instruction_button = pygame_menu.themes.Theme(widget_font=font1,
+                                              background_color=(0, 0, 0, 0),
+                                              widget_padding=25,
+                                              readonly_color=(255,255,255),
+                                              widget_font_color = (255,255,255),
+                                              widget_font_size=24,
+                                              title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
+                                              title_background_color=(0,0,0)
+                                              )
 
 instruction = pygame_menu.Menu(height=600,
-                        width=800,
-                        theme=instruction_button,
-                        title='Instruction'
-                        )
+                               width=800,
+                               theme=instruction_button,
+                               title='Instruction'
+                               )
 
 instruction.add.vertical_margin(1)
-instruction.add.label('How to play?',
-                 align=pygame_menu.locals.ALIGN_LEFT,
-                 font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
-                 font_size=44
-                 )
+instruction.add.label(title='How to play?',
+                      align=pygame_menu.locals.ALIGN_LEFT,
+                      font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
+                      font_size=44
+                      )
 how_to_play = ('Gra polega na zbijaniu bloczków, które znikają i dają po 1 punkcie. Masz 3 życia. Nie da się '
                'wygrać - walczysz z samym sobą o najlepszy rezultat. Poruszasz się strzałkami (LEFT, RIGHT),'
                ' a piłkę wyrzucasz spacją (SPACE). Po dotarciu do górnej części ekranu te wracają'
@@ -351,15 +362,15 @@ how_to_play = ('Gra polega na zbijaniu bloczków, które znikają i dają po 1 p
                'zwiększa się/zmniejsza się.\n'
                'Powodzenia!')
 
-instruction_info = instruction.add.label(how_to_play,
-                               max_char=45,
-                               align=pygame_menu.locals.ALIGN_CENTER,
-                               margin=(29, 1),
-                               font_size=24,
-                               font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
-                               font_color=(255, 255, 255),
-                               padding=0
-                               )
+instruction_info = instruction.add.label(title=how_to_play,
+                                         max_char=45,
+                                         align=pygame_menu.locals.ALIGN_CENTER,
+                                         margin=(29, 1),
+                                         font_size=24,
+                                         font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
+                                         font_color=(255, 255, 255),
+                                         padding=0
+                                         )
 
 DIFFICULTY = ['EASY']
 def set_difficulty(value, difficulty):
@@ -405,6 +416,7 @@ def start_the_game(difficulty):
                         arcade_ball_speed_x -= 5
                         arcade_ball_speed_y += 5
 
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     arcade_paddle_speed_x += 3
@@ -424,11 +436,18 @@ def start_the_game(difficulty):
         pygame.display.flip()
         clock.tick(60)
 
-menu.add.selector('Difficulty :', [('Easy', 'EASY'),
-                                  ('Medium', 'MEDIUM'),
-                                  ('Hard', 'HARD')],
+menu.add.selector(title='Difficulty :',
+                  items=[('Easy', 'EASY'),('Medium', 'MEDIUM'), ('Hard', 'HARD')],
                   onchange=set_difficulty
                   )
+settings.add.selector(title="Sound :",
+                      items=[('ON', True),('OFF', False)],
+                      onchange=sound_settings
+                      )
+settings.add.selector(title="Sound efects : ",
+                      items=[('ON', True),('OFF', False)],
+                      onchange=sound_effects_settings
+                      )
 menu.add.button('Play', start_the_game, DIFFICULTY)
 menu.add.button('Instruction', instruction)
 menu.add.button('Author', author)
