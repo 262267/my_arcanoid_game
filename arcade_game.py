782a615d620bfadf15dc84c955b153d99bcf5b48
262267 <81594625+262267@users.noreply.github.com>
2021-06-09 01:06:30 +0200

    ostatni gruby update
    - dodanie możliwości zmiany koloru bloczków, piłki oraz naszej paletki do odbijania
    - usunięcie "bugów" związanych z przywracaniem gry po zbiciu wszystkich klocków
    - kosmetyczne poprawki kodu

--- arcade_game.py
+++ arcade_game.py
@@ -9,8 +9,7 @@ tab = [[], [],
 
 
 def arcade_ball_movement():
-    global arcade_ball_speed_x, arcade_ball_speed_y, arcade_paddle_speed_x, \
-        score_value, lifes_value, best_value
+    global arcade_ball_speed_x, arcade_ball_speed_y
 
     arcade_ball.y -= arcade_ball_speed_y
     arcade_ball.x -= arcade_ball_speed_x
@@ -21,6 +20,12 @@ def arcade_ball_movement():
         arcade_ball_speed_x *= -1
 
     # collision - ball with paddle
+    ball_collision_with_paddle()
+    bounders()
+
+
+def ball_collision_with_paddle():
+    global arcade_ball_speed_x, arcade_ball_speed_y
     collision_tolerance = 10
     if arcade_ball.colliderect(arcade_paddle):
         if abs(arcade_paddle.top - arcade_ball.bottom) < collision_tolerance:
@@ -30,37 +35,42 @@ def arcade_ball_movement():
         if abs(arcade_paddle.right - arcade_ball.left) < collision_tolerance:
             arcade_ball_speed_x *= -1
 
+
+def bounders():
+    global arcade_ball_speed_x, arcade_ball_speed_y, life_value, best_value, space_click
+
     # bounders
     if arcade_paddle.left <= 0:
         arcade_paddle.left = 0
     if arcade_paddle.right >= screen_width:
         arcade_paddle.right = screen_width
+
     if arcade_ball.bottom >= screen_height:
         arcade_ball_speed_x = 0
         arcade_ball_speed_y = 0
         arcade_ball.x = arcade_paddle.x + 25
         arcade_ball.y = arcade_paddle.y - 30
-        pygame.mixer.music.load("graphics_and_sounds\\roblox.mp3")
+        pygame.mixer.music.load('graphics_and_sounds\\roblox.mp3')
         pygame.mixer.music.play()
         pygame.mixer.music.rewind()
-        lifes_value -= 1
-        if lifes_value == 0:
-            pygame.mixer.music.load("graphics_and_sounds\\end_game.wav")
+        life_value -= 1
+        if life_value == 0:
+            pygame.mixer.music.load('graphics_and_sounds\\end_game.wav')
             pygame.mixer.music.play()
             pygame.mixer.music.rewind()
             if score_value > best_value:
                 best_value = score_value
-            # score_value = 0
+        space_click = False
 
 
-def draw_rectangulars():
-    global arcade_ball_speed_y, score_value, arcade_ball_speed_x, best_value
+def draw_rectangles():
+    global arcade_ball_speed_y, score_value, arcade_ball_speed_x, best_value,\
+        win, space_click, arcade_paddle_speed_x
+
     # Drawing Rectangles
-
     for i in range(len(tab)):
         for j in range(len(tab[i])):
-            pygame.draw.rect(screen, color, tab[i][j])
-
+            pygame.draw.rect(screen, arcade_blocks_color, tab[i][j])
 
     end = True
     for x in tab:
@@ -70,12 +80,15 @@ def draw_rectangulars():
     if end:
         arcade_ball.x = 380
         arcade_ball.y = 495
-        start_the_game(DIFFICULTY)
+        arcade_paddle_speed_x = 0
         if score_value > best_value:
             best_value = score_value
+        win = True
+        space_click = False
+        start_the_game(DIFFICULTY)
 
     hit = False
-    element = tab[0][0]
+    element = None
     k = 0
     for i in range(len(tab)):
         for j in range(len(tab[i])):
@@ -92,8 +105,7 @@ def draw_rectangulars():
                     arcade_ball_speed_y *= -1
                     arcade_ball_speed_x *= -1
 
-
-                pygame.mixer.music.load("graphics_and_sounds\\click.wav")
+                pygame.mixer.music.load('graphics_and_sounds\\click.wav')
                 pygame.mixer.music.play()
                 pygame.mixer.music.rewind()
                 score_value += 1
@@ -114,22 +126,22 @@ scoreY = 400
 
 
 def show_score():
-    user_score_view = fontObj.render("Score: " + str(score_value),
+    user_score_view = fontObj.render('Score: ' + str(score_value),
                                      True, (255, 255, 255))
     screen.blit(user_score_view, (scoreX, scoreY))
 
 
 # life
-lifes_value = 3
+life_value = 3
 fontObj1 = pygame.font.SysFont('calibri', 28)
 lifeX = 640
 lifeY = 430
 
 
-def show_lifes():
-    user_lifes_view = fontObj1.render("Lifes: " + str(lifes_value),
+def show_life():
+    user_life_view = fontObj1.render('Life: ' + str(life_value),
                                       True, (255, 255, 255))
-    screen.blit(user_lifes_view, (lifeX, lifeY))
+    screen.blit(user_life_view, (lifeX, lifeY))
 
 
 # best score
@@ -139,7 +151,7 @@ bestY = 460
 
 
 def best_score():
-    user_best_value_view = fontObj1.render("Best score: " + str(best_value), True, (255, 255, 255))
+    user_best_value_view = fontObj1.render('Best score: ' + str(best_value), True, (255, 255, 255))
     screen.blit(user_best_value_view, (bestX, bestY))
 
 
@@ -157,34 +169,38 @@ clock = pygame.time.Clock()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-# initializing color for rectangle
-color = (255, 0, 255)
+
+# color for strings
 color2 = (255, 255, 255)
-color1 = (255, 0, 0)
+
+# color for blocks
+arcade_blocks_color = [255, 0, 255]
 
 # Title and Icon
-my_image = pygame.image.load("graphics_and_sounds\\tło.png")
-pygame.display.set_caption("arcade game created by Jakub Muzyka")
-icon = pygame.image.load("graphics_and_sounds\\arcanoid.png")
+my_image = pygame.image.load('graphics_and_sounds\\tło.png')
+pygame.display.set_caption('arcade game created by Jakub Muzyka')
+icon = pygame.image.load('graphics_and_sounds\\arcanoid.png')
 pygame.display.set_icon(icon)
 
 # arcanoid
 arcade_paddle = pygame.Rect(355, 530, 80, 10)
 arcade_paddle_speed_x = 0
+arcade_paddle_color = [255, 0, 255]
 
 # ball
 arcade_ball = pygame.Rect(380, 495, 30, 30)
 arcade_ball_speed_x = 0
 arcade_ball_speed_y = 0
+arcade_ball_color = [255, 255, 255]
 
 # adding music to menu
 begin = sound.Sound()
-pygame.mixer.music.load("graphics_and_sounds\\menu_music.wav")
+pygame.mixer.music.load('graphics_and_sounds\\menu_music.wav')
 pygame.mixer.music.play(-1)
-begin.set_sound(pygame_menu.sound.SOUND_TYPE_WIDGET_SELECTION, "graphics_and_sounds\\click.wav")
+begin.set_sound(pygame_menu.sound.SOUND_TYPE_WIDGET_SELECTION, 'graphics_and_sounds\\click.wav')
+
 # Disable a sound
 begin.set_sound(pygame_menu.sound.SOUND_TYPE_ERROR, None)
-
 
 # create a manu for our game
 font = pygame_menu.font.FONT_8BIT
@@ -207,7 +223,7 @@ menu = pygame_menu.Menu(title='welcome to the arcanoid game!',
                         theme=mytheme)
 
 menu.set_sound(begin, recursive=True)
-myimage = pygame_menu.baseimage.BaseImage(image_path="graphics_and_sounds\\tło.png")
+myimage = pygame_menu.baseimage.BaseImage(image_path='graphics_and_sounds\\tło.png')
 
 
 def main_background():
@@ -244,7 +260,6 @@ settings = pygame_menu.Menu(height=600,
                             theme=settings_menu,
                             title='Settings'
                             )
-
 
 def add_settings():
     settings.add.vertical_margin(50)
@@ -296,13 +311,6 @@ author_info = author.add.label(title=info,
                                padding=0
                                )
 
-# image_path = pygame_menu.baseimage.BaseImage(
-#     image_path="graphics_and_sounds\\ja1.png"
-# )
-# author.add_image(image_path,
-#                  align=pygame_menu.locals.ALIGN_CENTER,
-#                  border_inflate=(383, 250)
-#                  )
 
 for line in author_info:
     line.set_max_width(400)
@@ -330,6 +338,7 @@ instruction.add.label(title='How to play?',
                       font_name=pygame_menu.font.FONT_OPEN_SANS_BOLD,
                       font_size=44
                       )
+
 how_to_play = ('Gra polega na zbijaniu bloczków, które znikają i dają po 1 punkcie. Masz 3 życia. Nie da się '
                'wygrać - walczysz z samym sobą o najlepszy rezultat. Poruszasz się strzałkami (LEFT, RIGHT),'
                ' a piłkę wyrzucasz spacją (SPACE). Po dotarciu do górnej części ekranu te wracają'
@@ -352,6 +361,7 @@ instruction_info = instruction.add.label(title=how_to_play,
 # levels
 DIFFICULTY = ['EASY']
 
+
 def set_difficulty(value, difficulty):
     selected, index = value
     DIFFICULTY[0] = difficulty
@@ -367,22 +377,56 @@ def game_over_message1(msg, colorful):
     end_view1 = fontObj2.render(msg, True, colorful)
     screen.blit(end_view1, (220, 300))
 
+
 def game_over_message2(msg, colorful):
     end_view1 = fontObj2.render(msg, True, colorful)
     screen.blit(end_view1, (250, 400))
 
-def game_over_screen():
 
+def game_over_screen():
     game_over_message("Game Over! Your score: " + str(score_value), color2)
     game_over_message1("Press q to return to menu", color2)
     game_over_message2("Press p to play again", color2)
     pygame.display.update()
 
+
+def change_paddle_color(value1, colorp):
+    global arcade_paddle_color
+    colorc, _ = value1
+    if colorp == (-1, -1, -1):
+        colorp = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+    arcade_paddle_color[0] = colorp[0]
+    arcade_paddle_color[1] = colorp[1]
+    arcade_paddle_color[2] = colorp[2]
+
+
+def change_ball_color(value2, colorb):
+    global arcade_ball_color
+    colorx, _ = value2
+    if colorb == (-1, -1, -1):
+        colorb = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+    arcade_ball_color[0] = colorb[0]
+    arcade_ball_color[1] = colorb[1]
+    arcade_ball_color[2] = colorb[2]
+
+
+def change_blocks_color(value3, colorbl):
+    global arcade_blocks_color
+    colord, _ = value3
+    if colorbl == (-1, -1, -1):
+        colorbl = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+    arcade_blocks_color[0] = colorbl[0]
+    arcade_blocks_color[1] = colorbl[1]
+    arcade_blocks_color[2] = colorbl[2]
+
+
+win = False
+space_click = False
+
+
 def start_the_game(difficulty):
     global arcade_paddle_speed_x, arcade_ball_speed_y, arcade_ball_speed_x, score_value, best_value, \
-        lifes_value, arcade_paddle
-
-    # space_click = False
+        life_value, arcade_paddle, win, space_click
 
     arcade_paddle = pygame.Rect(355, 530, 130, 10)
     arcade_paddle_speed_x = 0
@@ -390,6 +434,8 @@ def start_the_game(difficulty):
     arcade_ball.y = 495
     arcade_ball_speed_x = 0
     arcade_ball_speed_y = 0
+    if not win:
+        score_value = 0
 
     yy = 5
     for i in range(len(tab)):
@@ -404,61 +450,65 @@ def start_the_game(difficulty):
     # Game Loop
     running = True
     game_over = False
+
     while running:
 
         # background
         screen.blit(my_image, (0, 0))
+
+        # game over screen
         while game_over == True:
             game_over_screen()
-            lifes_value = 3
-            score_value = 0
+            life_value = 3
             for event in pygame.event.get():
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_p:
+                        win = False
                         start_the_game(difficulty)
                     if event.key == pygame.K_q:
+                        win = False
                         menu.mainloop(screen, main_background)
+
+        # levels
+        difficulty = difficulty[0]
+        if difficulty == 'EASY':
+            arcade_paddle = pygame.Rect(355, 530, 130, 10)
+        elif difficulty == 'MEDIUM':
+            arcade_paddle = pygame.Rect(355, 530, 80, 10)
+        elif difficulty == 'HARD':
+            arcade_paddle = pygame.Rect(355, 530, 40, 10)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                space_click = False
 
             # set the movement
-
-            difficulty = difficulty[0]
-            if difficulty == 'EASY':
-                arcade_paddle = pygame.Rect(355, 530, 130, 10)
-            elif difficulty == 'MEDIUM':
-                arcade_paddle = pygame.Rect(355, 530, 100, 10)
-            elif difficulty == 'HARD':
-                arcade_paddle = pygame.Rect(355, 530, 80, 10)
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    arcade_paddle_speed_x -= 3
+                    arcade_paddle_speed_x -= 4.5
                 if event.key == pygame.K_RIGHT:
-                    arcade_paddle_speed_x += 3
-
+                    arcade_paddle_speed_x += 4.5
                 if event.key == K_SPACE:
-                    p = 0.5
-                    # if space_click == False:
-                    if random.random() < p:
-                        arcade_ball_speed_x += 5
-                        arcade_ball_speed_y += 5
 
-                    else:
-                        arcade_ball_speed_x -= 5
-                        arcade_ball_speed_y += 5
-                    # space_click = True
+                    if space_click == False:
+                        p = 0.5
+                        if random.random() < p:
+                            arcade_ball_speed_x += 5
+                            arcade_ball_speed_y += 5
 
+                        else:
+                            arcade_ball_speed_x -= 5
+                            arcade_ball_speed_y += 5
+                    space_click = True
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    arcade_paddle_speed_x += 3
+                    arcade_paddle_speed_x += 4.5
                 if event.key == pygame.K_RIGHT:
-                    arcade_paddle_speed_x -= 3
+                    arcade_paddle_speed_x -= 4.5
 
-        if lifes_value == 0:
+        if life_value == 0:
             game_over = True
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
@@ -467,14 +517,14 @@ def start_the_game(difficulty):
                     elif event.key == pygame.K_q:
                         game_over = False
 
-        draw_rectangulars()
+        draw_rectangles()
         show_score()
-        show_lifes()
+        show_life()
         best_score()
         arcade_ball_movement()
         arcade_paddle.x += arcade_paddle_speed_x
-        pygame.draw.ellipse(screen, color2, arcade_ball)
-        pygame.draw.rect(screen, color, arcade_paddle)
+        pygame.draw.ellipse(screen, arcade_ball_color, arcade_ball)
+        pygame.draw.rect(screen, arcade_paddle_color, arcade_paddle)
         pygame.display.update()
         pygame.display.flip()
         clock.tick(60)
@@ -484,13 +534,45 @@ menu.add.selector(title='Difficulty :',
                   items=[('Easy', 'EASY'), ('Medium', 'MEDIUM'), ('Hard', 'HARD')],
                   onchange=set_difficulty
                   )
-settings.add.selector(title="Sound :",
+
+settings.add.selector(title='Sound :',
                       items=[('ON', True), ('OFF', False)],
                       onchange=sound_settings
                       )
-settings.add.selector(title="Sound efects : ",
+
+settings.add.selector(title='Sound effects : ',
                       items=[('ON', True), ('OFF', False)],
                       onchange=sound_effects_settings
+                      )
+
+settings.add.selector(title='Change paddle color: ',
+                      items=[('Default', (255, 0, 255)),
+                             ('Random', (-1, -1, -1)),
+                             ('Black', (0, 0, 0)),
+                             ('Blue', (12, 12, 200)),
+                             ('Red', (255, 0, 0)),
+                             ('Orange', (255, 128, 0))],
+                      onchange=change_paddle_color,
+                      )
+
+settings.add.selector(title='Change ball color: ',
+                      items=[('Default', (255, 255, 255)),
+                             ('Random', (-1, -1, -1)),
+                             ('Black', (0, 0, 0)),
+                             ('Blue', (12, 12, 200)),
+                             ('Red', (255, 0, 0)),
+                             ('Orange', (255, 128, 0))],
+                      onchange=change_ball_color,
+                      )
+
+settings.add.selector(title='Change blocks color: ',
+                      items=[('Default', (255, 0, 255)),
+                             ('Random', (-1, -1, -1)),
+                             ('Black', (0, 0, 0)),
+                             ('Blue', (12, 12, 200)),
+                             ('Red', (255, 0, 0)),
+                             ('Orange', (255, 128, 0))],
+                      onchange=change_blocks_color,
                       )
 
 menu.add.button('Play', start_the_game, DIFFICULTY)
